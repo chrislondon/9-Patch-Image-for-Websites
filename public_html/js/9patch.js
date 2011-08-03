37127b78fe932580eb0706dee2d34f2fe5b82480
Chris London <clondon@deseretdigital.com>
2011-08-02 20:59:41 -0600

    added resizability and converted site to OOP

--- public_html/js/9patch.js
+++ public_html/js/9patch.js
@@ -1,135 +1,53 @@
 $(window).load(function() {
 	$('div').each(function(){
 		if ($(this).css('background-image').match(/\.9\.(png|gif)/i)) {
-			var oImgSrc = $(this).css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
-			
-			var originalBG = $(this).css("background-color");
-			$(this).css('background', 'none');
-			
-			// o is origin
-			var oImg = new Image();
-			oImg.src = oImgSrc;
-			oWidth = oImg.width;
-			oHeight = oImg.height;
-
-			// d is destination
-			var dCtx, dCanvas, dWidth, dHeight;
-			dWidth = $(this).outerWidth();
-			dHeight = $(this).outerHeight();
-			dCanvas = document.createElement('canvas');
-			dCtx = dCanvas.getContext('2d');
-
-			dCtx.drawImage(oImg, 0, 0);
-
-			var horizontalPieces = new Array();
-			var verticalPieces = new Array();
-			// each piece contains d|s, position, width
-
-			var staticColor, repeatColor;
-			// Loop over each  horizontal pixel and get piece
-			data = dCtx.getImageData(0, 0, oWidth, 1).data;
-			staticColor = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3];
-			
-			var tempLength = data.length - 4;
-			repeatColor = data[tempLength] + ',' + data[tempLength + 1] + ',' + 
-				data[tempLength + 2] + ',' + data[tempLength + 3];
-			horizontalPieces = getPieces(data, staticColor, repeatColor);
-			
-			// Loop over each  horizontal pixel and get piece
-			data = dCtx.getImageData(0, 0, 1, oHeight).data;
-			verticalPieces = getPieces(data, staticColor, repeatColor);
-
-			// use horizontalPieces and verticalPieces to generate image
-			dCanvas.width = dWidth;
-			dCanvas.height = dHeight;
-
-			var fillWidth, fillHeight;
-
-			// Determine the width for the static and dynamic pieces
-			var tempStaticWidth = 0;
-			var tempDynamicCount = 0;
-			
-			for (var i = 0, n = horizontalPieces.length; i < n; i++) {
-				if (horizontalPieces[i][0] == 's') {
-					tempStaticWidth += horizontalPieces[i][2];
-				} else {
-					tempDynamicCount++;
-				}
-			}
-
-			fillWidth = (dWidth - tempStaticWidth) / tempDynamicCount;
-
-			// Determine the height for the static and dynamic pieces
-			var tempStaticHeight = 0;
-			var tempDynamicCount = 0;
-			for (var i = 0, n = verticalPieces.length; i < n; i++) {
-				if (verticalPieces[i][0] == 's') {
-					tempStaticHeight += verticalPieces[i][2];
-				} else {
-					tempDynamicCount++;
-				}
-			}
-
-			fillHeight = (dHeight - tempStaticHeight) / tempDynamicCount;
-
-			// Loop through each of the vertical/horizontal pieces and draw on
-			// the canvas
-			for (var i = 0, m = verticalPieces.length; i < m; i++) {
-				for (var j = 0, n = horizontalPieces.length; j < n; j++) {
-					var tempFillWidth, tempFillHeight;
-
-					tempFillWidth = (horizontalPieces[j][0] == 'd') ? 
-							fillWidth : horizontalPieces[j][2];
-					tempFillHeight = (verticalPieces[i][0] == 'd') ? 
-							fillHeight : verticalPieces[i][2];
-
-					// Stretching :
-					if (verticalPieces[i][0] != 'r') {
-						// Stretching is the same function for the static squares
-						// the only difference is the widths/heights are the same.
-						dCtx.drawImage(oImg,
-						    horizontalPieces[j][1], verticalPieces[i][1],
-						    horizontalPieces[j][2], verticalPieces[i][2],
-						    0, 						0,
-						    tempFillWidth, tempFillHeight);
-					} else {
-						var tempCanvas    = document.createElement('canvas');
-						tempCanvas.width  = horizontalPieces[j][2];
-						tempCanvas.height = verticalPieces[i][2];
-						
-						var tempCtx = tempCanvas.getContext('2d');
-						tempCtx.drawImage(oImg,
-								horizontalPieces[j][1], verticalPieces[i][1],
-								horizontalPieces[j][2], verticalPieces[i][2],
-								0, 						0,
-								horizontalPieces[j][2], verticalPieces[i][2]);
-						
-						var tempPattern = dCtx.createPattern(tempCanvas, 'repeat');
-						dCtx.fillStyle = tempPattern;
-						dCtx.fillRect(
-								0, 			   0,
-								tempFillWidth, tempFillHeight);
-					}
-
-					// Shift to next x position
-					dCtx.translate(tempFillWidth, 0);
-				}
-
-				// shift back to 0 x and down to the next line
-				dCtx.translate(-dWidth, (verticalPieces[i][0] == 's' ? verticalPieces[i][2] : fillHeight));
-			}
-			
-			// store the canvas as the div's background
-			var url = dCanvas.toDataURL();
-			$(this).css("background", originalBG+" url("+url+")");
-			$(this).css("background-repeat", "no-repeat");
+			new NinePatch($(this));
 		}
 	});
-
-	return;
 });
 
-function getPieces(dataArray, staticColor, repeatColor) {
+function NinePatch(div) {
+	this.div = div;
+	this.bgImage = new Image();
+	this.bgImage.src = this.div.css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
+	this.originalBG = this.div.css("background-color");
+	this.div.css('background', 'none');
+	
+	// Create a temporary canvas to get the 9Patch index data.
+	var tempCtx, tempCanvas;
+	tempCanvas = document.createElement('canvas');
+	tempCtx = tempCanvas.getContext('2d');
+	tempCtx.drawImage(this.bgImage, 0, 0);
+
+	// Loop over each  horizontal pixel and get piece
+	var data = tempCtx.getImageData(0, 0, this.bgImage.width, 1).data;
+	
+	// Use the upper-left corner to get staticColor, use the upper-right corner
+	// to get the repeatColor.
+	var tempLength = data.length - 4;
+	var staticColor = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3];
+	var repeatColor = data[tempLength] + ',' + data[tempLength + 1] + ',' + 
+			data[tempLength + 2] + ',' + data[tempLength + 3];
+	
+	this.horizontalPieces = this.getPieces(data, staticColor, repeatColor);
+	
+	// Loop over each  horizontal pixel and get piece
+	data = tempCtx.getImageData(0, 0, 1, this.bgImage.height).data;
+	this.verticalPieces = this.getPieces(data, staticColor, repeatColor);
+
+	// use this.horizontalPieces and this.verticalPieces to generate image
+	
+	this.draw();
+	var _this = this;
+	this.div.resize(function(){_this.draw()});
+}
+NinePatch.prototype.div = null;
+NinePatch.prototype.originalBG = null;
+NinePatch.prototype.horizontalPieces = null;
+NinePatch.prototype.verticalPieces = null;
+NinePatch.prototype.bgImage = null;
+
+NinePatch.prototype.getPieces = function(data, staticColor, repeatColor) {
 	var tempDS, tempPosition, tempWidth, tempColor, tempType;
 	var tempArray = new Array();
 
@@ -156,4 +74,100 @@ function getPieces(dataArray, staticColor, repeatColor) {
 	tempArray.push(new Array(tempDS, tempPosition, tempWidth));
 
 	return tempArray;
+}
+
+NinePatch.prototype.draw = function() {
+	var dCtx, dCanvas, dWidth, dHeight;
+	dWidth = this.div.outerWidth();
+	dHeight = this.div.outerHeight();
+	dCanvas = document.createElement('canvas');
+	dCtx = dCanvas.getContext('2d');
+	
+	dCanvas.width = dWidth;
+	dCanvas.height = dHeight;
+	
+	var fillWidth, fillHeight;
+
+	// Determine the width for the static and dynamic pieces
+	var tempStaticWidth = 0;
+	var tempDynamicCount = 0;
+	
+	for (var i = 0, n = this.horizontalPieces.length; i < n; i++) {
+		if (this.horizontalPieces[i][0] == 's') {
+			tempStaticWidth += this.horizontalPieces[i][2];
+		} else {
+			tempDynamicCount++;
+		}
+	}
+
+	fillWidth = (dWidth - tempStaticWidth) / tempDynamicCount;
+
+	// Determine the height for the static and dynamic pieces
+	var tempStaticHeight = 0;
+	var tempDynamicCount = 0;
+	for (var i = 0, n = this.verticalPieces.length; i < n; i++) {
+		if (this.verticalPieces[i][0] == 's') {
+			tempStaticHeight += this.verticalPieces[i][2];
+		} else {
+			tempDynamicCount++;
+		}
+	}
+
+	fillHeight = (dHeight - tempStaticHeight) / tempDynamicCount;
+
+	console.log(dWidth);
+	// Loop through each of the vertical/horizontal pieces and draw on
+	// the canvas
+	for (var i = 0, m = this.verticalPieces.length; i < m; i++) {
+		for (var j = 0, n = this.horizontalPieces.length; j < n; j++) {
+			var tempFillWidth, tempFillHeight;
+
+			tempFillWidth = (this.horizontalPieces[j][0] == 'd') ? 
+					fillWidth : this.horizontalPieces[j][2];
+			tempFillHeight = (this.verticalPieces[i][0] == 'd') ? 
+					fillHeight : this.verticalPieces[i][2];
+
+			// Stretching :
+			if (this.verticalPieces[i][0] != 'r') {
+				// Stretching is the same function for the static squares
+				// the only difference is the widths/heights are the same.
+				console.log(this.bgImage);
+				dCtx.drawImage(
+						
+						this.bgImage,
+				    this.horizontalPieces[j][1], this.verticalPieces[i][1],
+				    this.horizontalPieces[j][2], this.verticalPieces[i][2],
+				    0, 						0,
+				    tempFillWidth, tempFillHeight);
+			} else {
+				var tempCanvas    = document.createElement('canvas');
+				tempCanvas.width  = this.horizontalPieces[j][2];
+				tempCanvas.height = this.verticalPieces[i][2];
+				
+				var tempCtx = tempCanvas.getContext('2d');
+				tempCtx.drawImage(this.bgImage,
+						this.horizontalPieces[j][1], this.verticalPieces[i][1],
+						this.horizontalPieces[j][2], this.verticalPieces[i][2],
+						0, 						0,
+						this.horizontalPieces[j][2], this.verticalPieces[i][2]);
+				
+				var tempPattern = dCtx.createPattern(tempCanvas, 'repeat');
+				dCtx.fillStyle = tempPattern;
+				dCtx.fillRect(
+						0, 			   0,
+						tempFillWidth, tempFillHeight);
+			}
+
+			// Shift to next x position
+			dCtx.translate(tempFillWidth, 0);
+		}
+
+		// shift back to 0 x and down to the next line
+		dCtx.translate(-dWidth, (this.verticalPieces[i][0] == 's' ? this.verticalPieces[i][2] : fillHeight));
+	}
+	
+	// store the canvas as the div's background
+	var url = dCanvas.toDataURL();
+	this.div.css("background", this.originalBG+" url("+url+")");
+	this.div.css("background-repeat", "no-repeat");
 }
